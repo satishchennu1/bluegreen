2ddeffc2271c2e5e3bf8881c2a92584abe200948
root <root@ip-10-201-26-228.uacslab.uspto.gov>
2019-09-02 04:41:17 +0000

    adding the files

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
               </tr>
 	      <tr>
                 <td>version</td>
-                <td><?php Version2 ?></td>
+                <td><?php "Version3" ?></td>
               </tr>
             </tbody>
           </table>
